4f82482de7162beff06b99901d2352e17b5c8a92
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-11-06 00:18:09 +0300

    feat: добавить каркас проекта

---
+++ src/main/java/com/github/valentina810/beauty_of_code_2/BeautyOfCode2Application.java
@@ -0,0 +1,13 @@
+package com.github.valentina810.beauty_of_code_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyOfCode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyOfCode2Application.class, args);
+	}
+
+}
